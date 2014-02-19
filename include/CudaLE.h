fe8db29c80982fad220a30c6076773c98547cd45
fizban007 <fizban007@gmail.com>
2014-02-19 15:54:12 -0500

    Renamed the main header to CudaLE

--- include/CudaLE.h
+++ include/CudaLE.h
@@ -1,22 +1,22 @@
 /////////////////////////////////////////////////////////////////////////////////////////
 ///
-///           \file  CudaET.h
+///           \file  CudaLE.h
 ///
-/// __Description__:     Main include file for the CudaET lib
+/// __Description__:     Main include file for the CudaLE lib
 ///
-/// __Last modified__:   <2014-02-19 13:32:33 alex>\n
+/// __Last modified__:   <2014-02-19 15:54:01 alex>\n
 /// __Version__:         1.0\n
 /// __Author__:          Alex Chen, fizban007@gmail.com\n
 /// __Organization__:    Columbia University
 ///
 /////////////////////////////////////////////////////////////////////////////////////////
 
-#ifndef  _CUDAET_H_
-#define  _CUDAET_H_
+#ifndef  _CUDALE_H_
+#define  _CUDALE_H_
 
 #include "cudaDef.h"
 #include "Operators.h"
 #include "Variable.h"
 #include "Functions.h"
 
-#endif   // ----- #ifndef _CUDAET_H_  ----- 
+#endif   // ----- #ifndef _CUDALE_H_  ----- 
