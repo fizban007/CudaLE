e3c02d6ea800536ecceb9a3f7bde7c9b16f92017
fizban007 <fizban007@gmail.com>
2014-02-19 19:40:51 -0500

    Added a cu testfile

---
+++ LEtest.cu
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<math.h>
+#include "CudaLE.h"
+
+using namespace CudaLE;
+
+// const CoordinateType type = COORD_CYLINDRICAL;
+
+// typedef UnaryOp<Id, double> Var;
+
+int main () 
+{
+    // Var _x(1), _y(2);
+    // (_sin(_1) + _2)::op_type myF = MakeFunc(_sin(_1) + _2);
+    auto myF = _sin(_1) + _2;
+    // std::cout << sin(2.0) + 3.0 << std::endl;
+    // std::cout << (_log(_1) * _exp(_2) + _3)(2.0, -1, 1.0) << std::endl;
+    std::cout << _sin(_pow<2>(_1) + _2)(2.0, 3.0) << std::endl;
+    std::cout << myF(1.0, 2.0) << std::endl;
+    return 0;
+}
