8819de1cb0a58a7bc756afeb8431cd91d11e48ac
fizban007 <fizban007@gmail.com>
2014-04-10 00:13:11 -0400

    Changed indentation

--- include/Functions.h
+++ include/Functions.h
@@ -4,133 +4,133 @@
 
 namespace CudaLE {
 
-    struct Sin
-    {
-        HOST_DEVICE static double apply(double x) {
-            return sin(x);
-        }
-    };
-
-    template <typename Arg>
-    UnaryOp<Sin, Arg> _sin(Arg arg) {
-        return UnaryOp<Sin, Arg>(arg);
+struct Sin
+{
+    HOST_DEVICE static double apply(double x) {
+        return sin(x);
     }
+};
 
-    struct Cos
-    {
-        HOST_DEVICE static double apply(double x) {
-            return cos(x);
-        }
-    };
+template <typename Arg>
+UnaryOp<Sin, Arg> _sin(Arg arg) {
+    return UnaryOp<Sin, Arg>(arg);
+}
 
-    template <typename Arg>
-    UnaryOp<Cos, Arg> _cos(Arg arg) {
-        return UnaryOp<Cos, Arg>(arg);
+struct Cos
+{
+    HOST_DEVICE static double apply(double x) {
+        return cos(x);
     }
+};
 
-    struct Exp
-    {
-        HOST_DEVICE static double apply(double x) {
-            return exp(x);
-        }
-    };
+template <typename Arg>
+UnaryOp<Cos, Arg> _cos(Arg arg) {
+    return UnaryOp<Cos, Arg>(arg);
+}
 
-    template <typename Arg>
-    UnaryOp<Exp, Arg> _exp(Arg arg) {
-        return UnaryOp<Exp, Arg>(arg);
+struct Exp
+{
+    HOST_DEVICE static double apply(double x) {
+        return exp(x);
     }
+};
 
-    struct Log
-    {
-        HOST_DEVICE static double apply(double x) {
-            return log(x);
-        }
-    };
+template <typename Arg>
+UnaryOp<Exp, Arg> _exp(Arg arg) {
+    return UnaryOp<Exp, Arg>(arg);
+}
 
-    template <typename Arg>
-    UnaryOp<Log, Arg> _log(Arg arg) {
-        return UnaryOp<Log, Arg>(arg);
+struct Log
+{
+    HOST_DEVICE static double apply(double x) {
+        return log(x);
     }
+};
 
-    struct Plus
-    {
-        HOST_DEVICE static double apply(double a, double b) {
-            return a + b;
-        }
-    };
+template <typename Arg>
+UnaryOp<Log, Arg> _log(Arg arg) {
+    return UnaryOp<Log, Arg>(arg);
+}
 
-    template <typename Left, typename Right>
-    BinaryOp<Plus, Left, Right> operator+ (Left lhs, Right rhs) {
-        return BinaryOp<Plus, Left, Right>(lhs, rhs);
+struct Plus
+{
+    HOST_DEVICE static double apply(double a, double b) {
+        return a + b;
     }
+};
 
-    struct Minus
-    {
-        HOST_DEVICE static double apply(double a, double b) {
-            return a - b;
-        }
-    };
+template <typename Left, typename Right>
+BinaryOp<Plus, Left, Right> operator+ (Left lhs, Right rhs) {
+    return BinaryOp<Plus, Left, Right>(lhs, rhs);
+}
 
-    template <typename Left, typename Right>
-    BinaryOp<Minus, Left, Right> operator- (Left lhs, Right rhs) {
-        return BinaryOp<Minus, Left, Right>(lhs, rhs);
+struct Minus
+{
+    HOST_DEVICE static double apply(double a, double b) {
+        return a - b;
     }
+};
 
-    struct Multiply
-    {
-        HOST_DEVICE static double apply(double a, double b) {
-            return a * b;
-        }
-    };
+template <typename Left, typename Right>
+BinaryOp<Minus, Left, Right> operator- (Left lhs, Right rhs) {
+    return BinaryOp<Minus, Left, Right>(lhs, rhs);
+}
 
-    template <typename Left, typename Right>
-    BinaryOp<Multiply, Left, Right> operator* (Left lhs, Right rhs) {
-        return BinaryOp<Multiply, Left, Right>(lhs, rhs);
+struct Multiply
+{
+    HOST_DEVICE static double apply(double a, double b) {
+        return a * b;
     }
+};
 
-    template <typename Left>
-    BinaryOp<Multiply, Left, ConstOp> operator* (Left lhs, double rhs) {
-        return BinaryOp<Multiply, Left, ConstOp>(lhs, ConstOp(rhs));
+template <typename Left, typename Right>
+BinaryOp<Multiply, Left, Right> operator* (Left lhs, Right rhs) {
+    return BinaryOp<Multiply, Left, Right>(lhs, rhs);
+}
+
+template <typename Left>
+BinaryOp<Multiply, Left, ConstOp> operator* (Left lhs, double rhs) {
+    return BinaryOp<Multiply, Left, ConstOp>(lhs, ConstOp(rhs));
+}
+
+template <typename Right>
+BinaryOp<Multiply, ConstOp, Right> operator* (double lhs, Right rhs) {
+    return BinaryOp<Multiply, ConstOp, Right>(ConstOp(lhs), rhs);
+}
+
+struct Divide
+{
+    HOST_DEVICE static double apply(double a, double b) {
+        return a / b;
     }
+};
 
-    template <typename Right>
-    BinaryOp<Multiply, ConstOp, Right> operator* (double lhs, Right rhs) {
-        return BinaryOp<Multiply, ConstOp, Right>(ConstOp(lhs), rhs);
+template <typename Left, typename Right>
+BinaryOp<Divide, Left, Right> operator/ (Left lhs, Right rhs) {
+    return BinaryOp<Divide, Left, Right>(lhs, rhs);
+}
+
+template <typename Left>
+BinaryOp<Divide, Left, ConstOp> operator/ (Left lhs, double rhs) {
+    return BinaryOp<Divide, Left, ConstOp>(lhs, ConstOp(rhs));
+}
+
+template <typename Right>
+BinaryOp<Divide, ConstOp, Right> operator/ (double lhs, Right rhs) {
+    return BinaryOp<Divide, ConstOp, Right>(ConstOp(lhs), rhs);
+}
+
+template<int power>
+struct Pow
+{
+    HOST_DEVICE static double apply(double a) {
+        return pow(a, power);
     }
+};
 
-    struct Divide
-    {
-        HOST_DEVICE static double apply(double a, double b) {
-            return a / b;
-        }
-    };
-
-    template <typename Left, typename Right>
-    BinaryOp<Divide, Left, Right> operator/ (Left lhs, Right rhs) {
-        return BinaryOp<Divide, Left, Right>(lhs, rhs);
-    }
-
-    template <typename Left>
-    BinaryOp<Divide, Left, ConstOp> operator/ (Left lhs, double rhs) {
-        return BinaryOp<Divide, Left, ConstOp>(lhs, ConstOp(rhs));
-    }
-
-    template <typename Right>
-    BinaryOp<Divide, ConstOp, Right> operator/ (double lhs, Right rhs) {
-        return BinaryOp<Divide, ConstOp, Right>(ConstOp(lhs), rhs);
-    }
-
-    template<int power>
-    struct Pow
-    {
-        HOST_DEVICE static double apply(double a) {
-            return pow(a, power);
-        }
-    };
-
-    template <int power, typename Arg>
-    UnaryOp<Pow<power>, Arg> _pow(Arg arg) {
-        return UnaryOp<Pow<power>, Arg>(arg);
-    }
+template <int power, typename Arg>
+UnaryOp<Pow<power>, Arg> _pow(Arg arg) {
+    return UnaryOp<Pow<power>, Arg>(arg);
+}
 
 }
