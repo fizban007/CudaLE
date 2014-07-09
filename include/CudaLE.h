ff200335651dc7a28a1ad9bc1b77251157146fcd
fizban007 <fizban007@gmail.com>
2014-07-08 23:40:58 -0400

    Changed include

--- include/CudaLE.h
+++ include/CudaLE.h
@@ -4,7 +4,7 @@
 ///
 /// __Description__:     Main include file for the CudaLE lib
 ///
-/// __Last modified__:   <2014-02-19 15:54:01 alex>\n
+/// __Last modified__:   <2014-07-08 19:14:31 alex>\n
 /// __Version__:         1.0\n
 /// __Author__:          Alex Chen, fizban007@gmail.com\n
 /// __Organization__:    Columbia University
@@ -14,9 +14,10 @@
 #ifndef  _CUDALE_H_
 #define  _CUDALE_H_
 
-#include "cudaDef.h"
+#include "cudaControl.h"
 #include "Operators.h"
 #include "Variable.h"
 #include "Functions.h"
+#include "Derivative.h"
 
 #endif   // ----- #ifndef _CUDALE_H_  ----- 
