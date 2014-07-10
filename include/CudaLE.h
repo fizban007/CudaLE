1ab147bbb99c7f47a468e01d2ac211e38f765a51
fizban007 <fizban007@gmail.com>
2014-07-10 12:58:43 -0400

    Changed formatting

--- include/CudaLE.h
+++ include/CudaLE.h
@@ -4,7 +4,7 @@
 ///
 /// __Description__:     Main include file for the CudaLE lib
 ///
-/// __Last modified__:   <2014-07-10 12:57:07 alex>\n
+/// __Last modified__:   <2014-07-10 12:58:27 alex>\n
 /// __Version__:         1.0\n
 /// __Author__:          Alex Chen, fizban007@gmail.com\n
 /// __Organization__:    Columbia University
@@ -20,8 +20,10 @@
 #include "Functions.h"
 #include "Derivative.h"
 
-#define DEFINE_FUNCTOR(NAME, FUNCTOR)      \
-    typedef typeof(FUNCTOR) type_ ## NAME; \
-    type_ ## NAME NAME
+#ifndef DEFINE_FUNCTOR
+  #define DEFINE_FUNCTOR(NAME, FUNCTOR)                  \
+      typedef typeof(FUNCTOR) type_ ## NAME;             \
+      type_ ## NAME NAME
+#endif
 
 #endif   // ----- #ifndef _CUDALE_H_  ----- 
