f51fa48d8877ff953fec1823536b7bc4c87d8904
fizban007 <fizban007@gmail.com>
2014-07-10 12:57:31 -0400

    Moved DEFINE_FUNCTOR macro definition here

--- include/CudaLE.h
+++ include/CudaLE.h
@@ -4,7 +4,7 @@
 ///
 /// __Description__:     Main include file for the CudaLE lib
 ///
-/// __Last modified__:   <2014-07-08 19:14:31 alex>\n
+/// __Last modified__:   <2014-07-10 12:57:07 alex>\n
 /// __Version__:         1.0\n
 /// __Author__:          Alex Chen, fizban007@gmail.com\n
 /// __Organization__:    Columbia University
@@ -19,5 +19,9 @@
 #include "Variable.h"
 #include "Functions.h"
 #include "Derivative.h"
+
+#define DEFINE_FUNCTOR(NAME, FUNCTOR)      \
+    typedef typeof(FUNCTOR) type_ ## NAME; \
+    type_ ## NAME NAME
 
 #endif   // ----- #ifndef _CUDALE_H_  ----- 
